589903ed1d0acfaa91c7b47d9aa5b45f2dc64168
Blackrush <blackrushx@gmail.com>
2015-07-02 01:33:23 +0200

    Implement a function mapping elements of a list

--- src/test/scala/spike/SpikeTest.scala
+++ src/test/scala/spike/SpikeTest.scala
@@ -21,6 +21,25 @@ class SpikeTest extends FunSpec {
       val IntExpression(n) = Interpreter(Macro(Parser(code)))
       assert(n == 55)
     }
+
+    it("should be possible to define a function mapping elements of a list") {
+      val code = """
+(defmacro defn (name args body) '(def #name (lambda #args #body)))
+(defmacro if (cond do else) '(cond (#cond #do) ('else #else)))
+
+(defn map (list fn)
+      (if list (cons (fn (hd list))
+                     (map (tl list)))
+               '()))
+
+(defn mul-2 (x)
+      (* x 2))
+
+(map '(1 2 3) mul-2)
+"""
+      val res = Interpreter(Macro(Parser(code)))
+      assert(res == ListExpression(List(IntExpression(2), IntExpression(4), IntExpression(6))))
+    }
   }
 }
 
